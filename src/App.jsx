4bd10d9c8d5b04ca14601bc04f8215cd1c0bcc61
dassag <dassagr.official@gmail.com>
2023-12-10 10:07:49 +0530

    props and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { CORE_CONCEPTS } from './data';
+
+export function CoreConcepts({ title, image, description }) {
+  return (
+    <li>
+      <img src={image} alt={title}></img>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
         <h1>React Essentials</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
@@ -10,7 +22,15 @@ function App() {
         </p>
       </header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
