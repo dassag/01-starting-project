f6f34109434528d79efcc83d474e89da8e93f02d
dassag <dassagr.official@gmail.com>
2023-12-21 09:18:05 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import { CORE_CONCEPTS } from './data';
-
-export function CoreConcepts({ title, image, description }) {
-  return (
-    <li>
-      <img src={image} alt={title}></img>
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </li>
-  );
-}
+import { useState } from 'react';
+import Results from './components/Results';
+import UserInput from './components/UserInput';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const isValidInput = userInput.duration >= 1;
+
+  function handleInputChange(inputIdentifier, newValue) {
+    //let intValue = parseInt(newValue, 10);
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <div>
-      <header>
-        <img src='src/assets/react-core-concepts.png' alt='Stylized atom' />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <section id='core-concepts'>
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]} />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-      </main>
-    </div>
+    <>
+      <UserInput userInput={userInput} onChange={handleInputChange} />
+      {isValidInput ? (
+        <Results input={userInput} />
+      ) : (
+        <p className='center'> Please enter valid duration</p>
+      )}
+    </>
   );
 }
 
